df52cb5b14cce647ada79c5dec3d6a9f3044c0e7
Drew Manley <andrewbmanley@gmail.com>
2022-11-04 20:26:57 -0500

    did a couple things
    Co-authored-by: Young Kim <57378483+ykim770@users.noreply.github.com>
    Co-authored-by: Rabea Ahmad <80486118+RabeaAhmad3@users.noreply.github.com>
    Co-authored-by: Hao Ze Lin <87346439+LinHAO-1@users.noreply.github.com>
    Co-authored-by: Kevin Tseng <107890711+Kevin-J-Tseng@users.noreply.github.com>

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -1,1 +1,16 @@
-// comment
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route, Links } from 'react-router-dom';
+
+
+// ONLY USE ROUTES
+const MainContainer = () => {
+  return (<BrowserRouter>
+  <Routes>
+    <Route path = "/home" element = {<Home />}></Route>
+    <Route path = "/MyProjects" element = {<MyProjects />}></Route>
+    <Route path = "/create" element = {<Create/>}></Route>
+    </Routes>
+  </BrowserRouter>)
+};
+
+export default MainContainer
